994c062bbafb64652b2bf340c6b65d34e3e9683e
manik <manik@LAPTOP-EP056MTR>
2026-09-08 10:30:27 +0530

    core java basics

---
+++ myfirstjavaproject/src/com/javaproject/Student3.java
@@ -0,0 +1,23 @@
+package com.javaproject;
+
+public class Student3 {
+	void display()
+	{
+	System.out.println("Name:Smith");
+	System.out.println("Age:24");
+	System.out.println("college:Standforrd university");
+	}
+	
+	
+
+	public static void main(String[] args) {
+		Student3 s3=new Student3();
+		s3.display();	
+		
+		
+		
+	
+
+	}
+
+}
