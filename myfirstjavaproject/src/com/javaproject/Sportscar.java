2e1381bfe35cf43fb59c3dfae7f0b6feb761ea5a
manik <manik@LAPTOP-EP056MTR.Dlink>
2026-09-08 16:11:42 +0530

    core java basics

---
+++ myfirstjavaproject/src/com/javaproject/Sportscar.java
@@ -0,0 +1,26 @@
+package com.javaproject;
+
+public class Sportscar {
+	String brand;
+	String model;
+	String carprice;
+	
+	void show()
+	{
+		System.out.println("Brand:"+"BMW");
+		System.out.println("Model:"+"M3");
+		System.out.println("Carprice:"+"2cr");
+		
+	}
+
+	public static void main(String[] args) {
+		Sportscar car=new Sportscar();
+		car.show();
+		
+		
+		
+		
+
+	}
+
+}
